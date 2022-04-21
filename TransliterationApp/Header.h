bc20235332db8d0fdf62c4354c233e95de907452
asdadsdasd <mkozarez12345@mail.ru>
2022-04-21 11:42:06 +0300

    Added Header.h and FunctionBodies.cpp

---
+++ TransliterationApp/Header.h
@@ -0,0 +1,27 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <ctype.h>
+#include <locale.h>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string inputFile(string fileName);
+
+vector<string> translit(string eng);
+
+void outputFile(vector<string> ru, string outputFileName);
+
+void setDecMap();
+
+string toupper(string line);
+
+char toupper(char ch);
+
+char tolower(char ch);
